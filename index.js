99dcfb1e495e1cffc2376c93372a12327414bc05
Oto Sales <ottosjr@hotmail.com>
2024-07-16 21:47:01 -0300

    exercicios da aula2

---
+++ index.js
@@ -0,0 +1,7 @@
+//alert("Hello World!!")
+
+let num1 = parseInt(prompt('Insira o primeiro número: '));
+let num2 = parseInt(prompt('Insira o segundo número: '));
+let soma = num1 + num2;
+console.log(soma);
+alert(`O resultado da soma é ${soma}`);
