68db679e5d8edfce5f9819c7cfa64fdbf6c8cef6
Oto Sales <ottosjr@hotmail.com>
2024-07-23 19:37:48 -0300

    adicionando exercício da aula 2

---
+++ condicional.js
@@ -0,0 +1,15 @@
+let num = parseInt(prompt("Insira um número de 1 a 5: "));
+
+if (num == 1) {
+    alert("Número um");
+} else if (num == 2) {
+    alert("Número dois");
+} else if (num == 3) {
+    alert("Número três");
+} else if (num == 4) {
+    alert("Número quatro");
+} else if (num == 5) {
+    alert("Número cinco");
+} else {
+    alert ("Número não informado!")
+}
---
+++ num-text.js
@@ -0,0 +1,5 @@
+let nome = prompt('Informe seu nome: ');
+let idade = parseInt(prompt('Informe sua idade: '));
+
+console.log(`Olá! Eu sou ${nome}, e tenho ${idade} anos!`);
+alert(`Olá! Eu sou ${nome}, e tenho ${idade} anos!`);
